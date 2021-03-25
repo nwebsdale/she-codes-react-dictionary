5e521df841bf6d662581133d2c26d10299e3bcc5
nwebsdale <natashawebsdale@hotmail.co.uk>
2021-03-25 19:02:20 +0000

    Initital Commit and Setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="header row">
+          <h1>The Online Dictionary</h1>
+        </header>
+        <footer>Coded by me :)</footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
